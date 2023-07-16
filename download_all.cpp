a16103cb9d2d913aaa9dcbb865a32db9a7ecc69f
Hana Dusíková <hanicka@hanicka.net>
2023-07-16 19:33:29 +0200

    remove unused code

--- download_all.cpp
+++ download_all.cpp
@@ -4,21 +4,6 @@
 #include <numeric>
 #include <utility/format.hpp>
 
-auto download_all() -> toolbox::task<size_t> {
-	try {
-		auto index = co_await toolbox::simple_fetch("https://hanickadot.github.io/index2.html");
-		std::cout << "index2.html is " << toolbox::data_size(index.size()) << "\n";
-	} catch (...) {
-		std::cout << "Missing index2.html\n";
-	}
-
-	auto index = toolbox::simple_fetch("https://hanickadot.github.io/index.html");
-	auto image1 = toolbox::simple_fetch("https://hanickadot.github.io/new-york-2022/img/FPL03986.jpg");
-	auto image2 = toolbox::simple_fetch("https://hanickadot.github.io/new-york-2022/img/FPL04038.jpg");
-
-	co_return (co_await index).size() + (co_await image1).size() + (co_await image2).size();
-}
-
 auto all_done(std::vector<toolbox::fetch_task<std::vector<std::byte>>> in) -> toolbox::task<std::vector<std::vector<std::byte>>> {
 	auto result = std::vector<std::vector<std::byte>>{};
 
@@ -41,7 +26,7 @@ auto download_files(std::vector<std::string> in) -> std::vector<toolbox::fetch_t
 	return result;
 }
 
-auto intermediate() -> toolbox::task<void> {
+auto process() -> toolbox::task<void> {
 	// size_t total = co_await download_all();
 	// std::cout << "Downloaded " << toolbox::data_size(total) << " total.\n";
 	//
@@ -53,7 +38,7 @@ auto intermediate() -> toolbox::task<void> {
 
 int main() {
 	try {
-		intermediate().get();
+		process().get();
 	} catch (...) {
 		std::cout << "mooo\n";
 	}
